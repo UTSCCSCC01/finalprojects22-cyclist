3842c6925528f0ca35eacc20ece98ebdccdde667
Dane <dane.gled@gmail.com>
2022-06-07 15:47:27 -0400

    Implement Top Navigation Bar and Page Routing

--- frontend/CycList/src/app/app-routing.module.ts
+++ frontend/CycList/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DailyViewComponent } from './daily-view/daily-view.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'app-daily-view', component: DailyViewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
