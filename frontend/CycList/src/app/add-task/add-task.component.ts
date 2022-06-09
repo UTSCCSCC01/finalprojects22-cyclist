b2ac3f8b65d214b936e1a92ee0a288314b1957d4
Andrew D'Amario <andrew@thecross.ca>
2022-06-09 05:54:07 -0400

    Create Add form and connect it to Angular form object.

--- frontend/CycList/src/app/add-task/add-task.component.ts
+++ frontend/CycList/src/app/add-task/add-task.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, INJECTOR, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
@@ -7,9 +8,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTaskComponent implements OnInit {
 
-  constructor() { }
+  formActive = true;
+
+  constructor(
+    private fb: FormBuilder
+    ) {
+  }
 
   ngOnInit(): void {
+  }
+
+  addTaskForm() {
+    this.formActive = true;
+  }
+
+  overlay() {
+    this.formActive = false;
+  }
+
+  // @ViewChild("addTask") addTask: ElementRef<HTMLElement>;
+
+  form: FormGroup = this.fb.group({
+    signifier: [null],          // maybe just call it type????????
+    content: [null],
+    dueDate: [null],            // please merge     day: Int month: Int year: Int
+    dueTime: [null],
+    startDate: [null],
+    startTime: [null],
+    description: [null],
+
+    expectedDuration: [null],  // pointless because we have start and due/end unless this is an AI value
+
+    repeat: [null],         // maybe just repeat true of false
+    frequency: [null],
+    dayWeekMonthYear: [null],   // add year?
+    repeatStartDay: [null],     // pointless????????
+
+    group: [null],                // maybe just call tag, group, was this what was meant????????
+    priority: [null],         // maybe like options: ! !! or !!!    
+    mood: [null],
+    location: [null],
+    interests: [null],        // ?????????
+
+    reminders: [null],     // TODO: need to be able to have multiple so maybe an array of 
+    collaborations: [null]  // TODO: add friends that you will do that job with
+
+                            // Ideas: file (image), url
+  });
+
+  submitForm() {
+    this.formActive = false;
+    console.log(this.form.value)
+    // TODO: send data to back end
+
+    // TODO: get all tasks from backend again
+    // this.getDailyTasks(3, 6, 2022);
+    this.form.reset();
   }
 
 }
