e36762f5e0129d545018395be5ccab587348ec14
Jeffrey he <jeffreyhe2001@gmail.com>
2022-07-01 14:13:16 -0400

    CYC-94: created the for loop structure to display dates and their tasks.

--- frontend/CycList/src/app/daily-log/daily-log.component.ts
+++ frontend/CycList/src/app/daily-log/daily-log.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalsService } from '../globals.service';
 
 @Component({
   selector: 'app-daily-log',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DailyLogComponent implements OnInit {
 
-  constructor() { }
+  dailyTasks : any;
+  numDates = 6;   // TODO: dependency injection!?
+  dates : Date[] = [];
+
+  constructor() {}
 
   ngOnInit(): void {
+
+    //get the upcomming `this.numDates` dates
+    let today = new Date();
+    
+    for (let i = 0; i < this.numDates; i++) {
+      this.dates[i] = new Date();
+      this.dates[i].setDate(today.getDate() + i);
+    }
+
+    // this.tasks = GlobalsService.getDailyTasks();
+
+    let day1 = ["[test]1a", "[test]1b", "[test]1c", "[test]1d"];
+    let day2 : string[] = [];
+    let day3 = ["[test]1a"];
+    let day4 = ["[test]1a", "[test]1d"];
+    let day5 : string[] = [];
+    let day6 : string[] = [];
+
+    this.dailyTasks = [day1, day2, day3, day4, day5, day6];
+
+    
   }
 
 }
