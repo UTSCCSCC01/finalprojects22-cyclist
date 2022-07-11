e211461e69b5f7ed30ace0e2b1814cdc90f9f30d
Dane <dane.gled@gmail.com>
2022-07-11 18:24:33 -0400

    CYC-176: Implement the feature to view the future log starting at the current month

--- frontend/CycList/src/app/future-log/future-log.component.ts
+++ frontend/CycList/src/app/future-log/future-log.component.ts
@@ -7,8 +7,20 @@ import { GlobalsService } from '../globals.service';
   styleUrls: ['./future-log.component.scss']
 })
 export class FutureLogComponent implements OnInit {
+  month;
+  // monthName;
+  monthNames;
 
-  constructor(public globals: GlobalsService) {}
+  constructor(public globals: GlobalsService) {
+    let today = new Date();
+    this.month = today.getMonth() + 1
+
+    this.monthNames = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+
+    // this.monthName = this.monthNames[this.month - 1];
+  }
   
   async ngOnInit() {
     this.globals.getFutureLogTasks();
