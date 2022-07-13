1f5c326bb923bff62680aed306acded1d1b6a2cd
Tianpai Z <octainfila@live.com>
2022-07-13 13:39:06 -0400

    CYC-133 show tasks on the right hand side
    create split screen and display tasks on the right hand side. Tasks are not filter to meet the monthly log requirement. just displaying tasks works

--- frontend/CycList/src/app/monthly-log/monthly-log.component.ts
+++ frontend/CycList/src/app/monthly-log/monthly-log.component.ts
@@ -4,31 +4,34 @@ import { GlobalsService } from '../globals.service';
 @Component({
   selector: 'app-monthly-log',
   templateUrl: './monthly-log.component.html',
-  styleUrls: ['./monthly-log.component.scss']
+  styleUrls: ['./monthly-log.component.scss'],
 })
 export class MonthlyLogComponent implements OnInit {
-  month;
-  monthName;
-  
+  month: number;
+  monthName: string;
+  allDaysInMonth: Date[];
+  rowsInCalendar: number;
+
   constructor(public globals: GlobalsService) {
-    let today = new Date();
-    this.month = today.getMonth() + 1
-
-    const monthNames = ["January", "February", "March", "April", "May", "June",
-      "July", "August", "September", "October", "November", "December"
-    ];
-
-    this.monthName = monthNames[this.month - 1];
+    this.month = new Date().getMonth() + 1;
+    this.monthName = new Date().toLocaleString('default', { month: 'long' });
+    this.allDaysInMonth = this.getDaysInMonth(
+      this.month,
+      new Date().getFullYear()
+    );
+    this.rowsInCalendar = Math.ceil(this.allDaysInMonth.length / 7);
   }
-  
-  
+
+  getDaysInMonth = (month: number, year: number) =>
+    new Array(31)
+      .fill('')
+      .map((v, i) => new Date(year, month - 1, i + 1))
+      .filter((v) => v.getMonth() === month - 1);
 
   async ngOnInit() {
     this.globals.getMonthlyLogTasks();
-    this.globals.curLog = "monthly";
+    this.globals.curLog = 'monthly';
   }
 
-  refreshTasks() {
-  }
-
+  refreshTasks() {}
 }
